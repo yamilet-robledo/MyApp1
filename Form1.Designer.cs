1e046f2b68d80541ec384601b904062fae24e159
yamilet-robledo <esmeralda.robledo0202@gmail.com>
2026-09-21 11:08:22 -0600

    Repo principal

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,9 +32,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Mi primer App";
             this.ResumeLayout(false);
